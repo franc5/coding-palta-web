248b0f679ab53d47e6bd25a504a175fcf3697baf
Francisco Costa <francosta@protonmail.com>
2022-01-26 07:52:43 -0300

    Add empty sections

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,9 @@ import "../assets/materialize/styles/styles.css";
 function NavbarLinks(): JSX.Element {
   return (
     <>
-      <li><Link to="#">Services</Link></li>
-      <li><Link to="#">About Us</Link></li>
-      <li><Link to="#">Contact</Link></li>
+      <li><Link to="/services">Services</Link></li>
+      <li><Link to="/about">About Us</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
     </>
   );
 }
