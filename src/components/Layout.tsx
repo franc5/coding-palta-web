436e64f7da00847fd5b0d271ccbbcd2d7b0f5047
Francisco Costa <francosta@protonmail.com>
2022-01-26 05:52:26 -0300

    Add a basic (still in progress) Layout component which imports Materialize styles

---
+++ src/components/Layout.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import "../assets/materialize/styles/materialize.min.css";
+import "../assets/materialize/styles/styles.css";
+
+function NavbarLinks(): JSX.Element {
+  return (
+    <>
+      <li><Link to="#">Services</Link></li>
+      <li><Link to="#">About Us</Link></li>
+      <li><Link to="#">Contact</Link></li>
+    </>
+  );
+}
+
+interface Props {
+  children: JSX.Element;
+}
+
+export default function Layout({ children }: Props): JSX.Element {
+  return (
+    <>
+      <nav className="white" role="navigation">
+        <div className="nav-wrapper container">
+          <Link id="logo-container" to="/" className="brand-logo">CodingPalta</Link>
+
+          <ul className="right hide-on-med-and-down">
+            <NavbarLinks />
+          </ul>
+
+          <ul id="nav-mobile" className="sidenav">
+            <NavbarLinks />
+          </ul>
+          <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+
+        </div>
+      </nav>
+      {children}
+      {/** Footer */}
+    </>
+  );
+}
+
