cd2574933aba3a053e07ad3a349ccfe352f4a71b
franc5 <64472239+franc5@users.noreply.github.com>
2022-02-06 21:39:17 +0000

    Deploying to gh-pages from @ franc5/coding-palta-web@07509fc24cc44ec4906faceb0633846e8203959c 🚀

---
+++ component---src-pages-404-tsx-1e451e9278bea509cee3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcoding_palta=self.webpackChunkcoding_palta||[]).push([[218],{938:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var l=t(7294),a=t(5444);function r(){return l.createElement("main",null,l.createElement("div",null,"404"),l.createElement("br",null),l.createElement(a.rU,{to:"/"},"Go home"),".")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-1e451e9278bea509cee3.js.map
