337ba5394b2e697a96c1043e4de9d9a78e4f818e
Francisco Costa <francosta@protonmail.com>
2022-02-07 13:22:58 +0100

    Add a Divider component and some minor changes to Home page

---
+++ src/components/Divider.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Divider() {
+  return (
+    <div className="container">
+      <div className="section">
+        <div className="divider" />
+      </div>
+    </div>
+  );
+}
+
--- src/pages/index.en.tsx
+++ src/pages/index.en.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import * as _global from "../global";
 
+import Divider from "../components/Divider";
 import OneTopicSection from "../components/OneTopicSection";
 
 // TODO: Load images using Gatsby images plugin
@@ -21,7 +22,7 @@ export default function IndexPage() {
           <div className="container">
             <h1 className="header center teal-text text-lighten-2">CodingPalta</h1>
             <div className="row center">
-              <h3 className="header col s12 light">Nearshore software development</h3>
+              <h3 className="header col s12 light">Professional software development for startups</h3>
             </div>
           </div>
         </div>
@@ -40,7 +41,7 @@ export default function IndexPage() {
                   We are a team of professional software developers with more than six years of experience in web and mobile development.
                 </p>
                 <p>
-                  We provide software development solutions to startups seeking to improve their productions using the newest technologies and the best practices.
+                  We provide software development solutions for startups seeking to improve their products using the newest technologies and the best practices of the industry.
                 </p>
                 <p>
                   Our work is supported by agile methodologies such as Scrum, always tailored to each different client and project.
@@ -51,11 +52,7 @@ export default function IndexPage() {
         </div>
       </div>
 
-      <div className="container">
-        <div className="section">
-          <div className="divider" />
-        </div>
-      </div>
+      <Divider />
 
       <div className="container">
         <div className="section">
@@ -64,7 +61,7 @@ export default function IndexPage() {
             description={
               <>
                 <p>
-                  We work together with <a href="https://woxlab.com" target="_blank">WoxLab LLC</a> with which we share some common clients and we are constantly seeking for new clients, always keeping our focus on startups.
+                  We work with <a href="https://woxlab.com" target="_blank">WoxLab LLC</a> with which we share some common clients. And we are constantly seeking for new startups to work with.
                 </p>
                 <a href="https://woxlab.com" target="_blank">
                   <img src="https://woxlab.com/wp-content/uploads/2021/01/Woxlab.Logo-02.png" alt="WoxLab" width="252px" height="72px" />
