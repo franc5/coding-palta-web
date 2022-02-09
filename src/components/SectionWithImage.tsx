c4cb70f075c3b09749be31bfa9b54ee4a250fabe
Francisco Costa <francosta@protonmail.com>
2022-02-09 15:28:44 +0100

    Add a (yet dummy) section to About Us

--- src/components/SectionWithImage.tsx
+++ src/components/SectionWithImage.tsx
@@ -3,17 +3,23 @@ import React from "react";
 interface Props {
   children: JSX.Element;
   image: string;
+  imageInTheLeft?: boolean;
 }
 
-  export default function SectionWithImage({ children, image }: Props) {
+export default function SectionWithImage({ children, image, imageInTheLeft = false }: Props) {
+  const content = (
+    <div className="col l6 s12">
+      {children}
+    </div>
+  );
+
   return (
     <div className="row">
-      <div className="col l6 s12">
-        {children}
-      </div>
+      {!imageInTheLeft && content}
       <div className="col l6 hide-on-med-and-down">
         <img className="responsive-img" src={image} />
       </div>
+      {imageInTheLeft && content}
     </div>
   );
 }
--- src/pages/about.en.tsx
+++ src/pages/about.en.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import OneTopicSection from "../components/OneTopicSection";
+import Divider from "../components/Divider";
 import SectionWithImage from "../components/SectionWithImage";
 
 import About_0 from "../images/About_0.jpg";
+import About_1 from "../images/About_1.jpg";
 import { ReactComponent as LinkedIn_Logo } from "../images/linkedin.svg";
 
 const LINKED_IN_LOGO_SIZE = 15;
@@ -16,16 +18,32 @@ const LinkedInLink = () => (
 
 export default function AboutPage() {
   return (
-    <OneTopicSection title="About CodingPalta">
-      <SectionWithImage image={About_0}>
-        <p className="left-align">
-          CodingPalta is a is a technology company specialized in software development.
-          It was founded in 2022 by Eng. Luis Francisco Costa ( <LinkedInLink /> ) aiming to be a company which provides software development solutions for startups.
-          <br />
-          To reach these goals, we use the newest technologies and follow the best practice in order to develop high quality software.
-        </p>
-      </SectionWithImage>
-    </OneTopicSection>
+    <>
+      <OneTopicSection title="About CodingPalta">
+        <SectionWithImage image={About_0}>
+          <p className="left-align">
+            <b>CodingPalta</b> is a is a technology company specialized in software development.
+            <br />
+            It was founded in 2022 by <b>Eng. Luis Francisco Costa</b> ( <LinkedInLink /> ) aiming to be a company which provides software development solutions for startups.
+            <br />
+            To reach these goals, we use the newest technologies and follow the best practice in order to develop high quality software.
+            <br />
+            We have extensive experience working with startups and we know how to help them grow.
+          </p>
+        </SectionWithImage>
+      </OneTopicSection>
+
+      <Divider />
+
+      <OneTopicSection title="How we wok?">
+        <SectionWithImage image={About_1} imageInTheLeft>
+          <p className="left-align">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+        </SectionWithImage>
+      </OneTopicSection>
+
+    </>
   );
 }
 
