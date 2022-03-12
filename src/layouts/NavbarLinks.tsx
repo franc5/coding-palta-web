356e2495d64292691e4cf03cf9eb29488aaa4036
Francisco Costa <francosta@protonmail.com>
2022-03-12 11:56:57 +0100

    Add a list of specific services to the NavBar

--- src/layouts/NavbarLinks.tsx
+++ src/layouts/NavbarLinks.tsx
@@ -12,7 +12,16 @@ const LOCALE = {
   [LANGS.EN]: {
     "services": {
       "text": "Services",
-      "url": "/services",
+      "list": [{
+        "text": "Mobile apps",
+        "url": "/mobileapps",
+      }, {
+        "text": "Web apps",
+        "url": "/webapps",
+      }, {
+        "text": "Consulting",
+        "url": "/consulting",
+      }],
     },
     "about": {
       "text": "About Us",
@@ -32,10 +41,20 @@ const LOCALE = {
       "url": "/es",
     },
   },
+
   [LANGS.ES]: {
     "services": {
       "text": "Servicios",
-      "url": "/es/servicios",
+      "list": [{
+        "text": "Apps móviles",
+        "url": "/es/appsmoviles",
+      }, {
+        "text": "Apps web",
+        "url": "/es/appsweb",
+      }, {
+        "text": "Consultoría",
+        "url": "/es/consultoria",
+      }],
     },
     "about": {
       "text": "Nosotros",
@@ -57,11 +76,47 @@ const LOCALE = {
   },
 }
 
+interface ServicesMenuProps {
+  lang: LANGS;
+  show?: boolean;
+  smallScreen?: boolean;
+  closeSideNav?: () => void;
+}
+
+function ServicesMenu({ lang, show = true, smallScreen = false, closeSideNav }: ServicesMenuProps) {
+  const largeScreenStyles = {
+    display: show ? "block" : "none",
+    position: "absolute",
+    width: "200px",
+    backgroundColor: "#fff",
+    zIndex: "1000",
+    boxShadow: "0 20px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%), 0 1px 5px 0 rgb(0 0 0 / 20%)"
+  }
+
+  const smallScreenStyles = {}
+
+  return (
+    <ul style={!smallScreen ? largeScreenStyles : smallScreenStyles}>
+      {LOCALE[lang].services.list.map(service => (
+        <li
+          key={service.url}
+          style={{ width: "100%" }}
+          onClick={closeSideNav}
+        >
+          <Link to={service.url}>{service.text}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 interface NavbarLinksProps {
   lang: LANGS;
 }
 
 export default function NavbarLinks({ lang }: NavbarLinksProps): JSX.Element {
+  const [showServicesMenu, setShowServiceMenu] = React.useState<boolean>(false);
+
   const closeSideNav = () => {
     const sideNav = document.querySelector('.sidenav');
     window.M.Sidenav.getInstance(sideNav)?.close();
@@ -74,7 +129,27 @@ export default function NavbarLinks({ lang }: NavbarLinksProps): JSX.Element {
 
   return (
     <>
-      <li onClick={closeSideNav}><Link to={LOCALE[lang].services.url}>{LOCALE[lang].services.text}</Link></li>
+      <li
+        className="hide-on-med-and-down"
+        style={{ position: "relative" }}
+        onMouseOver={() => setShowServiceMenu(true)}
+        onMouseLeave={() => setShowServiceMenu(false)}
+      >
+        <a>
+          {LOCALE[lang].services.text}
+          <i className="material-icons right">arrow_drop_down</i>
+        </a>
+        <ServicesMenu lang={lang} show={showServicesMenu} />
+      </li>
+
+      <li className="hide-on-large-only">
+        <ServicesMenu
+          lang={lang}
+          smallScreen
+          closeSideNav={closeSideNav}
+        />
+      </li>
+
       <li onClick={closeSideNav}><Link to={LOCALE[lang].about.url}>{LOCALE[lang].about.text}</Link></li>
       <li onClick={showToastMessage}><Link to="#">{LOCALE[lang].blog.text}<sup>({LOCALE[lang].blog.text_2})</sup></Link></li>
       <li onClick={closeSideNav}><Link to={LOCALE[lang].contact.url}>{LOCALE[lang].contact.text}</Link></li>
